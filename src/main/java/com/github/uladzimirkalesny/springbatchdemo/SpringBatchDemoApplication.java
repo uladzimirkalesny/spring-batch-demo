2e5d2459aabfd4b4b0609ca5aeca1f8e2ed74a0d
Uladzimir Kalesny <uladzimirkalesny@gmail.com>
2022-12-21 23:21:47 +0300

    [5.3] - Writing to a Database with PreparedStatement

--- src/main/java/com/github/uladzimirkalesny/springbatchdemo/SpringBatchDemoApplication.java
+++ src/main/java/com/github/uladzimirkalesny/springbatchdemo/SpringBatchDemoApplication.java
@@ -7,25 +7,26 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
 import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
-import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.Date;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchDemoApplication {
 
-    public static String[] NAMES = {"orderId", "firstName", "lastName", "email", "cost", "itemId", "itemName", "shipDate"};
+    public static String INSERT_ORDER_SQL =
+            "INSERT INTO orders_output(order_id, first_name, last_name, email, item_id, item_name, cost, ship_date)" +
+                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -84,19 +85,25 @@ public class SpringBatchDemoApplication {
     }
 
     @Bean
-    public ItemWriter<Order> flatFileItemWriter() {
-        BeanWrapperFieldExtractor<Order> beanWrapperFieldExtractor = new BeanWrapperFieldExtractor<>();
-        beanWrapperFieldExtractor.setNames(NAMES);
+    public ItemWriter<Order> jdbcBatchItemWriterBuilder() {
+        return new JdbcBatchItemWriterBuilder<Order>()
+                .dataSource(dataSource)
+                .sql(INSERT_ORDER_SQL)
+                .itemPreparedStatementSetter(fillPreparedStatement())
+                .build();
+    }
 
-        DelimitedLineAggregator<Order> delimitedLineAggregator = new DelimitedLineAggregator<>();
-        delimitedLineAggregator.setDelimiter(",");
-        delimitedLineAggregator.setFieldExtractor(beanWrapperFieldExtractor);
-
-        FlatFileItemWriter<Order> flatFileItemWriter = new FlatFileItemWriter<>();
-        flatFileItemWriter.setResource(new FileSystemResource("/Users/Uladzimir_Kalesny/Downloads/orders.csv"));
-        flatFileItemWriter.setLineAggregator(delimitedLineAggregator);
-
-        return flatFileItemWriter;
+    private ItemPreparedStatementSetter<Order> fillPreparedStatement() {
+        return (order, ps) -> {
+            ps.setLong(1, order.getOrderId());
+            ps.setString(2, order.getFirstName());
+            ps.setString(3, order.getLastName());
+            ps.setString(4, order.getEmail());
+            ps.setString(5, order.getItemId());
+            ps.setString(6, order.getItemName());
+            ps.setBigDecimal(7, order.getCost());
+            ps.setDate(8, new Date(order.getShipDate().getTime()));
+        };
     }
 
     @Bean
@@ -104,7 +111,7 @@ public class SpringBatchDemoApplication {
         return this.stepBuilderFactory.get("readFlatFileStep")
                 .<Order, Order>chunk(2)
                 .reader(itemReader())
-                .writer(flatFileItemWriter())
+                .writer(jdbcBatchItemWriterBuilder())
                 .build();
     }
 
--- src/main/resources/orders.sql
+++ src/main/resources/orders.sql
@@ -16,3 +16,15 @@ INSERT INTO orders (order_id, first_name, last_name, email, cost, item_id, item_
 VALUES (2, 'Anna', 'Doe', 'anna.doe@gmail.com', '1', '2', 'potato', '2022-12-15');
 INSERT INTO orders (order_id, first_name, last_name, email, cost, item_id, item_name, ship_date)
 VALUES (3, 'Brad', 'Doe', 'brad.doe@gmail.com', '1', '3', 'beer', '2022-12-14');
+
+CREATE TABLE orders_output
+(
+    order_id   BIGINT,
+    first_name TEXT,
+    last_name  TEXT,
+    email      TEXT,
+    cost       TEXT,
+    item_id    TEXT,
+    item_name  TEXT,
+    ship_date  DATE
+);
